b964f651151a7b17f903340a6797cb885dcb407d
Edgar <edpires4@sapo.pt>
2021-05-10 00:55:05 +0100

    New Project

---
+++ classes_objects/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/edpires/github/Cpp-exemples/classes_objects
+# Build directory: /home/edpires/github/Cpp-exemples/classes_objects/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
