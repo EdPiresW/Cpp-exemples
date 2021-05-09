f065075030a4cdb12b60c7b8448ae63bc6cb599f
Edgar <edpires4@sapo.pt>
2021-05-10 00:55:05 +0100

    New Project

---
+++ classes_objects/build/CMakeFiles/constructor_parameters_default_values.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/constructor_parameters_default_values.dir/main.cpp.o"
+  "constructor_parameters_default_values"
+  "constructor_parameters_default_values.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/constructor_parameters_default_values.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ classes_objects/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+
+    
+return 0;
+}
+
